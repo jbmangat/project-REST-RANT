1f46b88e9a59b05de60af46283a576285169d0c8
Jagbir Mangat <jaguarsandhu@gmail.com>
2022-09-22 19:44:34 -0700

    adding styles to rest-rant

--- controllers/places.js
+++ controllers/places.js
@@ -17,6 +17,4 @@ router.get('/', (req, res) => {
     res.render('places/index', {places})
 })
 
-
-
 module.exports = router
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-
+// Modules and globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// express settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// controllers and routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
@@ -16,5 +20,6 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
+// listen for connections
 app.listen(process.env.PORT)
 // colors maybe brown orange yellow?
