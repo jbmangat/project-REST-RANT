e10ccee3e6e4a1e1fd682ab07498d42773d8ee4f
Jagbir Mangat <jaguarsandhu@gmail.com>
2022-09-28 16:36:11 -0700

    jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -1,9 +1,15 @@
 const router = require('express').Router()
 
+
+router.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('POST /places')
+})
+    
+
 router.get('/new', (req, res) => {
     res.render('places/new')
-  })
-  
+})
 
 router.get('/', (req, res) => {
     let places = [{
@@ -21,5 +27,9 @@ router.get('/', (req, res) => {
     }]      
     res.render('places/index', {places})
 })
+
+// router.post('/', (req, res) => {
+//     res.render('/places')
+// })
 
 module.exports = router
