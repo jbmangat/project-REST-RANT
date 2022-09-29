d67122aea7bf36dfa32431546b888f944fedf39d
Jagbir Mangat <jaguarsandhu@gmail.com>
2022-09-28 18:11:52 -0700

    show route connected

---
+++ views/places/show.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+const Def = require('../default')
+
+function show () {
+    console.log('------')
+    return (
+        <Def>
+          <main>
+            <h1>Show Page</h1>
+          </main>
+        </Def>
+    )
+}
+
+module.exports = show
