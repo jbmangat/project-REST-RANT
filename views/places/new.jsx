cec55fa151de4819aac8a4e79d54d75ef0f740aa
Jagbir Mangat <jaguarsandhu@gmail.com>
2022-09-30 18:02:17 -0700

    installed mongoose and used .then and .create to render index page

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -9,19 +9,32 @@ function new_form() {
         <form method="POST" action="/places">
           <div className="form-group">
             <label htmlFor="name">Place Name</label>
-            <input className="form-control" id="name" name="name" required />
+            <input 
+              className="form-control" 
+              id="name" 
+              name="name" 
+              required />
           </div>
           <div className="form-group">
             <label htmlFor="pic">Place Picture</label>
-            <input className="form-control" id="pic" name="pic" />
+            <input 
+              className="form-control" 
+              id="pic" 
+              name="pic" />
           </div>
           <div className="form-group">
             <label htmlFor="city">City</label>
-            <input className="form-control" id="city" name="city" />
+            <input 
+              className="form-control" 
+              id="city" 
+              name="city" />
           </div>
           <div className="form-group">
             <label htmlFor="state">State</label>
-            <input className="form-control" id="state" name="state" />
+            <input 
+              className="form-control" 
+              id="state" 
+              name="state" />
           </div>
           <div className="form-group">
             <label htmlFor="cuisines">Cuisines</label>
@@ -30,6 +43,14 @@ function new_form() {
               id="cuisines"
               name="cuisines"
               required
+            />
+          </div>
+          <div>
+            <label htmlFor="founded">Founded Year</label>
+            <input 
+              className="form-control" 
+              id="founded" 
+              name="founded" 
             />
           </div>
           <input className="btn btn-primary" type="submit" value="Add Place" />
