5de03fc3d0a450a2a7535039c96cf5ae37574002
Jagbir Mangat <jaguarsandhu@gmail.com>
2022-10-03 19:57:07 -0700

    added seeder and show establish method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -9,9 +9,11 @@ function show (data) {
             <h1>{data.place.name}</h1>
             <h2>Rating</h2>
               <p>Not Rated</p>
-            <h2>Description</h2>
-              <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</p>
             <img src={data.place.pic} alt={data.place.name} />
+            <h3>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</h3>
+            <h2>Description</h2>
+            <h3>{data.place.showEstablished()}</h3>
+            <h4>Serving {data.place.cuisines}</h4>
             <h2>Comments</h2>
               <p>No comments yet</p>
           </main>
